732d35715da8e56dbdab2b133bce8cc588394ffd
Jonah Largen <jhlargen2@gmail.com>
2025-08-05 23:32:07 -0400

    Command Tracing

---
+++ Skycamp.ApiService/Common/Middleware/CommandTracingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using FastEndpoints;
+using Skycamp.ApiService.Common.Telemetry;
+using System.Diagnostics;
+
+namespace Skycamp.ApiService.Common.Middleware;
+
+public class CommandTracingMiddleware<TCommand, TResult> : ICommandMiddleware<TCommand, TResult>
+    where TCommand : ICommand<TResult>
+{
+    public CommandTracingMiddleware()
+    {
+        
+    }
+
+    public async Task<TResult> ExecuteAsync(TCommand command, CommandDelegate<TResult> next, CancellationToken ct)
+    {
+        using var activity = TelemetryConstants.ActivitySource.StartActivity(command.GetType().Name, ActivityKind.Internal);
+
+        activity?.SetTag("otel.library.name", TelemetryConstants.ActivitySourceName);
+        activity?.SetTag("command.type", command.GetType().FullName);
+        activity?.SetTag("command.name", command.GetType().Name);
+        activity?.SetTag("result.type", typeof(TResult).FullName);
+
+        try
+        {
+            activity?.AddEvent(new ActivityEvent("Command executing"));
+
+            var result = await next();
+
+            activity?.AddEvent(new ActivityEvent("Command executed"));
+            activity?.SetStatus(ActivityStatusCode.Ok);
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            activity?.AddEvent(new ActivityEvent("Command failed"));
+            activity?.SetStatus(ActivityStatusCode.Error);
+            activity?.SetTag("exception.type", ex.GetType().FullName);
+            activity?.SetTag("exception.message", ex.Message);
+            activity?.SetTag("exception.stacktrace", ex.StackTrace);
+            throw;
+        }
+    }
+}
---
+++ Skycamp.ApiService/Common/Telemetry/TelemetryConstants.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+namespace Skycamp.ApiService.Common.Telemetry;
+
+public static class TelemetryConstants
+{
+    public const string ActivitySourceName = "Skycamp.ApiService";
+    public static readonly ActivitySource ActivitySource = new(ActivitySourceName);
+}
