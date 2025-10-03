895714ee18a66f1c24568e2c80ceba20b64aeeae
Jonah Largen <jhlargen2@gmail.com>
2025-10-02 21:03:32 -0400

    Working auth0

--- Skycamp.Web/Program.cs
+++ Skycamp.Web/Program.cs
@@ -1,7 +1,32 @@
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Skycamp.Web;
 using Skycamp.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Auth0 authentication
+var auth0Domain = builder.Configuration["Auth0:Domain"];
+var auth0ClientId = builder.Configuration["Auth0:ClientId"];
+
+if (string.IsNullOrEmpty(auth0Domain) || string.IsNullOrEmpty(auth0ClientId))
+{
+    throw new InvalidOperationException("Auth0 configuration is missing. Please ensure Auth0:Domain and Auth0:ClientId are set in the configuration.");
+}
+
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = auth0Domain;
+    options.ClientId = auth0ClientId;
+});
+
+builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.LoginPath = "/login";
+    options.ReturnUrlParameter = "redirectUri";
+});
+
+builder.Services.AddCascadingAuthenticationState();
 
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
@@ -10,6 +35,7 @@ builder.AddRedisOutputCache("cache");
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
 
 builder.Services.AddHttpClient<WeatherApiClient>(client =>
     {
@@ -35,8 +61,12 @@ app.UseOutputCache();
 
 app.MapStaticAssets();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.MapRazorPages();
 
 app.MapDefaultEndpoints();
 
