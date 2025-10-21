222c7379f911b39385665b4fff07817973638d8f
Jonah Largen <jhlargen2@gmail.com>
2025-10-20 21:54:18 -0400

    Add Azure Service Bus to AppHost configuration
    Integrated Azure Service Bus with topic and subscription support into the AppHost setup. Updated dependencies to include Aspire.Hosting.Azure.ServiceBus. Adjusted resource lifetimes and references for improved service orchestration.

--- Skycamp.AppHost/AppHost.cs
+++ Skycamp.AppHost/AppHost.cs
@@ -1,18 +1,38 @@
-var builder = DistributedApplication.CreateBuilder(args);
+using Microsoft.Extensions.Hosting;
 
-var cache = builder.AddRedis("cache");
+var builder = DistributedApplication.CreateBuilder(args);
+var isDevelopment = builder.Environment.IsDevelopment();
+
+var cache = builder.AddRedis("cache")
+    .WithLifetime(ContainerLifetime.Persistent);
 
 #pragma warning disable ASPIREPROXYENDPOINTS001
+
 var sql = builder.AddSqlServer("sqldb")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithEndpointProxySupport(false) //https://github.com/dotnet/aspire/issues/7046
     .WithDataVolume();
+
 #pragma warning restore ASPIREPROXYENDPOINTS001
 
 var db = sql.AddDatabase("database");
 
+var serviceBus = builder.AddAzureServiceBus(isDevelopment ? "sbemulatorns" : "messaging")
+    .RunAsEmulator(config =>
+    {
+        config.WithLifetime(ContainerLifetime.Persistent);
+    });
+
+serviceBus.AddServiceBusTopic("outbox")
+    .AddServiceBusSubscription("outbox-subscription");
+
 var apiService = builder.AddProject<Projects.Skycamp_ApiService>("apiservice")
     .WithReference(db)
+    .WaitFor(db)
+    .WithReference(cache)
+    .WaitFor(cache)
+    .WithReference(serviceBus)
+    .WaitFor(serviceBus)
     .WithHttpHealthCheck("/health")
     .WithEnvironment("Auth0__Domain", builder.Configuration["Auth0:Domain"])
     .WithEnvironment("Auth0__ClientId", builder.Configuration["Auth0:ClientId"])
@@ -23,7 +43,6 @@ builder.AddProject<Projects.Skycamp_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
     .WithReference(cache)
-    .WaitFor(cache)
     .WithReference(apiService)
     .WaitFor(apiService)
     .WithEnvironment("Auth0__Domain", builder.Configuration["Auth0:Domain"])
